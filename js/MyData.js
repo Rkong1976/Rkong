d1e9e3cbb13ea467a8cc9c2e80edfd3b7ec3149b
Alex Little <opttj2521@gmail.com>
2024-10-26 23:21:05 +0700

    next stage

---
+++ js/MyData.js
@@ -0,0 +1,39 @@
+class MyData {
+    constructor() {
+        this.itemsOne = [
+            {
+                path: "item (1).jpg",
+                title: "lsfasdd",
+                info: "a"
+            },
+            {
+                path: "item (2).jpg",
+                title: "lsfasdd",
+                info: "b"
+            },
+            {
+                path: "item (3).jpg",
+                title: "lsfasdd",
+                info: "c"
+            }
+        ];
+
+        this.itemsTwo = [
+            {
+                path: "item (29).jpg",
+                title: "lsfasdd",
+                info: "x"
+            },
+            {
+                path: "item (30).jpg",
+                title: "lsfasdd",
+                info: "y"
+            },
+            {
+                path: "item (31).jpg",
+                title: "lsfasdd",
+                info: "z"
+            }
+        ];
+    }
+}
