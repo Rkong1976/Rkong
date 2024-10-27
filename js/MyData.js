8eac50cd845644ffc28c668f72583802d73fd6cd
Alex Little <opttj2521@gmail.com>
2024-10-27 22:27:07 +0700

    next

--- js/MyData.js
+++ js/MyData.js
@@ -15,6 +15,131 @@ class MyData {
                 path: "item (3).jpg",
                 title: "lsfasdd",
                 info: "c"
+            },
+            {
+                path: "item (4).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (5).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (6).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (7).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (8).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (9).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (10).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (11).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (12).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (13).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (14).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (15).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (16).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (17).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (18).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (19).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (20).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (21).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (22).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (23).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (24).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (25).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (26).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (27).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (28).jpg",
+                title: "lsfasdd",
+                info: "c"
             }
         ];
 
@@ -33,6 +158,131 @@ class MyData {
                 path: "item (31).jpg",
                 title: "lsfasdd",
                 info: "z"
+            },
+            {
+                path: "item (32).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (33).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (34).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (35).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (36).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (37).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (38).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (39).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (40).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (41).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (42).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (43).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (44).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (45).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (46).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (47).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (48).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (49).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (50).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (51).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (52).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (53).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (54).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (55).jpg",
+                title: "lsfasdd",
+                info: "c"
+            },
+            {
+                path: "item (56).jpg",
+                title: "lsfasdd",
+                info: "c"
             }
         ];
     }
